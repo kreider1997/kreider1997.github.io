b47213c59f85735ddde0fef7892c1f71cddbf201
Keith Kreider <kreider1997@att.net>
2014-12-08 15:41:20 -0500

    added links in the footer and fixed margins that blew up as a result

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 
+ jQuery(function($){
+     $( '.ham' ).click(function(){
+     $('.hamMenu').addClass('expand')
+     $('.ham').addClass('btn-none')
+     })
+
+     $( '.close-btn' ).click(function(){
+     $('.hamMenu').removeClass('expand')
+     $('.ham').removeClass('btn-none')
+     })
+   })
+
+
+ jQuery(function($){
+    	     $( '.ham' ).click(function(){
+    	     $('.hamMenu').toggleClass('expand')
+    	     })
+        })
